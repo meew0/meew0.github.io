992c4f310ec0e22c3c0026ac5df5e2a87461a3e2
meew0 <blactbt@live.de>
2014-12-08 10:41:13 +0100

    Update diff.js

--- diff/diff.js
+++ diff/diff.js
@@ -86,10 +86,10 @@ var things = [
     katex.renderToString("\\left(" + max + "|" + calc(max) + "\\right)") + " ist ein lokales Maximum",
     katex.renderToString("\\left(" + min + "|" + calc(min) + "\\right)") + " ist ein lokales Minimum",
     katex.renderToString("(" + randoms[1] + "|" + calc(randoms[1]) + ")") + " ist Punkt der ersten Ableitung",
-    "Der Punkt " + katex.renderToString("(" + randoms[0] + "|" + calc(randoms[0]) + ")") + " hat eine Steigung von " + katex.renderToString(calc_(randoms[0]))
+    "Der Punkt " + katex.renderToString("(" + randoms[0] + "|" + calc(randoms[0]) + ")") + " hat eine Steigung von " + katex.renderToString("" + calc_(randoms[0]))
   ],
   [
-    katex.renderToString("f\\left(" + w + "\\right)") + " ist ein Wendepunkt, die zugehörige Wendetangente hat eine Steigung von " + katex.renderToString(calc_(w)),
+    katex.renderToString("f\\left(" + w + "\\right)") + " ist ein Wendepunkt, die zugehörige Wendetangente hat eine Steigung von " + katex.renderToString("" + calc_(w)),
     "Die zweite Ableitung hat einen " + katex.renderToString("y") + "-Achsenabschnitt von " + katex.renderToString("" + b__),
     katex.renderToString("(" + randoms[2] + "|" + calc(randoms[2]) + ")") + " ist Punkt der zweiten Ableitung"
   ]
